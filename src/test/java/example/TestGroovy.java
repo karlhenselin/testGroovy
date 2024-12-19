6f86384efcec96be54bde0537e774269b75e2e93
Karl Henselin <henselkp@gmail.com>
2024-12-19 10:51:49 -0600

    Added second test to compare. Run one at a time

--- src/test/java/example/TestGroovy.java
+++ src/test/java/example/TestGroovy.java
@@ -1,19 +1,22 @@
 package example;
 
-import groovy.lang.Binding;
-import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyShell;
 import groovy.lang.Script;
-import org.codehaus.groovy.control.CompilerConfiguration;
 import org.codehaus.groovy.control.MultipleCompilationErrorsException;
-import org.codehaus.groovy.control.ParserPluginFactory;
 import org.junit.Test;
 
 public class TestGroovy {
     @Test(expected = MultipleCompilationErrorsException.class)
-    public void testFailing2() {
+    public void testFailingNewWayDoesNotWork() {
         // Expect the next line to throw a compilation error
-        Script script = new GroovyShell().parse("this is not valid;asdf;asdf;asdf;asdf;asdf ");
+        System.setProperty("groovy.antlr4", "true");
+        new GroovyShell().parse("this is not valid;asdf;asdf;asdf;asdf;asdf ");
 
+    }
+
+    @Test(expected = MultipleCompilationErrorsException.class)
+    public void testFailingOldWay() {
+        System.setProperty("groovy.antlr4", "false");
+        new GroovyShell().parse("this is not valid;asdf;asdf;asdf;asdf;asdf ");
     }
 }
